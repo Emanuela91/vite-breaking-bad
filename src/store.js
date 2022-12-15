e5a9aa1406bc625f163d986e2a7c0b67fb373555
Emanuela91 <emanuela.marson91@gmail.com>
2022-12-15 18:20:36 +0100

    aggiunto del css in general

--- src/store.js
+++ src/store.js
@@ -2,5 +2,6 @@ import { reactive } from 'vue';
 
 export const store = reactive({
     characterList: [],
+    // in http aggiunta la 's' perchè aggiunge la modalità 'sicura' di navigazione, altrimenti mi da errore
     apiURL: "https://rickandmortyapi.com/api/character",
 });
