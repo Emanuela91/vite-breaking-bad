d5a378cb1d6e1aa73f3b531a5a6c1eaa6abc945d
Emanuela91 <emanuela.marson91@gmail.com>
2022-12-15 16:01:10 +0100

    aggiunto header con titolo, e inserito in app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+import HeaderVue from './components/Header.vue';
+import { store } from './store.js';
 
+export default {
+  name: "App",
+  components: {
+    HeaderVue
+  },
+
+  data() {
+    return {
+      store,
+    }
+  },
+}
 </script>
 
 <template>
-
+  <HeaderVue msg="Rick and Morty App" />
 </template>
 
 <style lang="scss">
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "Header",
+    props: ["msg"]
+}
+</script>
+
+<template>
+    <header class="container">
+        <h1 class="text-center my-5">
+            {{ msg }}
+        </h1>
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    font-weight: bold;
+}
+</style>
